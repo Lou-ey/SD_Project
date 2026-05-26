3f20a4a64cd81e2e5a9d47669bbbce5ba2fcb5bb
luis <camposfrancisco2002@gmail.com>
2026-05-26 16:25:07 +0100

    feat: Class ServerCon para comunicacao com o servidor

--- src/client/network/ServerCon.java
+++ src/client/network/ServerCon.java
@@ -1,4 +1,51 @@
 package client.network;
 
-public class ServerCon {
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+
+public class ServerCon implements Runnable {
+    private int port;
+    private String ip;
+    private DataOutputStream out;
+    private DataInputStream in;
+
+    private boolean connected;
+
+    public ServerCon(int port, String ip, boolean connected) {
+        this.port = port;
+        this.ip = ip;
+        this.connected = connected;
+    }
+
+    public void connect() {
+
+        out = new DataOutputStream(System.out);
+        in = new DataInputStream(System.in);
+
+        try {
+            while (connected) {
+                String msg = in.readUTF();
+                processServerMessage(msg);
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (connected) {
+                String msg = in.readUTF();
+                processServerMessage(msg);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void processServerMessage(String msg) {
+
+    }
 }
