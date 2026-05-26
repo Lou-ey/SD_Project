a76e9cc1d5f05610e14d606eb8ba1a070d938d57
Gabriel Ribeiro <gabino09022002@gmail.com>
2026-05-26 19:05:45 +0100

    chore: juntar funcionalidades, fase de testes

--- src/client/ClientMain.java
+++ src/client/ClientMain.java
@@ -1,4 +1,17 @@
 package client;
 
-public class ClientMain {
+import client.network.ServerConThread;
+
+public class ClientMain{
+    public static void main() {
+
+    String ip = "localhost";
+    int porta = 4000;
+
+
+    ServerConThread serverConThread = new ServerConThread(porta, ip);
+    serverConThread.start();
+
+    }
 }
+
